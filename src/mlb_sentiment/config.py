6dc3ce99f0c873da3ef5c946f9fbeb567b414166
Gregory Matousek <gamatousek@gmail.com>
2025-09-08 11:38:08 -0400

    Flake pyproject usage

--- src/mlb_sentiment/config.py
+++ src/mlb_sentiment/config.py
@@ -8,10 +8,10 @@ REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 REDDIT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
 REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT")
 
+
 def load_reddit_client():
     return praw.Reddit(
         client_id=REDDIT_CLIENT_ID,
         client_secret=REDDIT_SECRET,
-        user_agent=REDDIT_USER_AGENT
+        user_agent=REDDIT_USER_AGENT,
     )
-
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,15 +1,19 @@
 from mlb_sentiment import config
 import praw
 
+
 def test_reddit_connection():
     reddit = config.load_reddit_client()
     assert reddit is not None
     assert isinstance(reddit, praw.Reddit)
     assert reddit.read_only is True
 
+
 def test_fetch_submission():
     reddit = config.load_reddit_client()
-    submission = reddit.submission(url="https://www.reddit.com/r/NewYorkMets/comments/1nb31aw/post_game_thread_the_mets_fell_to_the_reds_by_a/")
+    submission = reddit.submission(
+        url="https://www.reddit.com/r/NewYorkMets/comments/1nb31aw/post_game_thread_the_mets_fell_to_the_reds_by_a/"
+    )
     assert submission is not None
     assert isinstance(submission, praw.models.Submission)
     assert isinstance(submission.title, str)
