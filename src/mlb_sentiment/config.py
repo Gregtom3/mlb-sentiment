96b8ac8a1fed8157ef22e93a771634d758142922
Gregory Matousek <gamatousek@gmail.com>
2025-09-08 11:19:33 -0400

    Basic commit, config

---
+++ src/mlb_sentiment/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+import praw
+
+load_dotenv()
+
+REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
+REDDIT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
+REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT")
+
+def load_reddit_client():
+    return praw.Reddit(
+        client_id=REDDIT_CLIENT_ID,
+        client_secret=REDDIT_SECRET,
+        user_agent=REDDIT_USER_AGENT
+    )
+
