8f935b06b024fc17bb0254cc6e31aaa33908caf9
Gregory Matousek <gamatousek@gmail.com>
2025-09-15 14:30:06 -0400

    Rename database 2

--- src/mlb_sentiment/models/dataloader.py
+++ src/mlb_sentiment/models/dataloader.py
@@ -9,7 +9,7 @@ def load_comments():
     Returns:
         list: A list of strings, where each string is the text of a comment.
     """
-    conn = sqlite3.connect("reddit.db")
+    conn = sqlite3.connect("MyDatabase.db")
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
 
